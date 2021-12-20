5ae2e120b9a02f22602c04f6baf7cae4c3daf1d4
Kenneth DAquila <kfd182@gmail.com>
2021-12-20 00:12:29 -0600

    Rename folder for web_api

--- src/web/api_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .pizza_controller import PizzaController
-
-urlpatterns = [
-    path('pizza', PizzaController.as_view(), name='index'),
-    path('pizza/<int:pizza_id>', PizzaController.as_view(), name='index'),
-]
